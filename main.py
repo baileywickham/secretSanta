16fc76db5dd99669de632debc95c47d72362e778
bailey <baileywickham@gmail.com>
2017-11-30 13:27:04 -0800

    update, still doesn't work

--- main.py
+++ main.py
@@ -5,24 +5,37 @@ import random
 # auth_token = "YTA0ZWFjZjM5NTNlOGJkMzllMzYwN2Y3NzI5M2Zi"
 # p = plivo.RestAPI(auth_id, auth_token)
 
-switch = []
+shuffledNames = []
 names = ["andy", "jack", "christian", "ben", "ian", "bailey"]
 numbers =  ["5033329523", "5037060383", "5034844734", "5038940386", "5037079976", "5039892243"]
+
+counter = 0
 
 
 def rand():
     tmpNames = list(names)
-    random.shuffle(tmpNames)
-    return tmpNames
+    # random.shuffle(tmpNames)
+    for index, n in enumerate(tmpNames):
+        randomNum = random.randrange(0, len(tmpNames))
+        print(randomNum)
+        shuffledNames.append(tmpNames[randomNum])
+
+    return shuffledNames
 
 
 def main():
     tmpNames = rand()
-
-    for idx, name in enumerate(names):
-        if name == tmpNames[idx]:
-            if idx != 0 and tmpNames[idx] != 
-    print(switch)
+    while True:
+        for idx, name in enumerate(names):
+            if name == tmpNames[idx]:
+                counter = 0
+                rand()
+                print('reseting...')
+            else:
+                counter += 1
+                print(counter)
+                if counter == 5:
+                    break
     print(l)
 
 
