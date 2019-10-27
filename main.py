3fad07afba7fedc58b27973d5469c36f94f20b0a
baileywickham <baileywickham@gmail.com>
2019-10-27 16:00:22 -0700

    remove api keys

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import random
 import plivo
 
-auth_id = "MAYJNHN2E3YJA0ZTKYMJ"
-auth_token = "MjJiYjk5ZjU4ZmExNmMxOWM4MjVhNTVmNGE3Yjg3"
+auth_id = ""
+auth_token = ""
 plv = plivo.RestClient(auth_id, auth_token)
+
 
 class Person:
     def __init__(self, name, number):
         self.name = name
         self.number = number
 
+
 def printResults(peoples):
     outstr = ''
     random.shuffle(peoples)
     f = open('output_women', 'w')
     for num in range(len(peoples)):
-        outstr += peoples[num % len(peoples)].name + ' >> ' + peoples[(num+1) % len(peoples)].name + '\n'
-        textResults(peoples[(num+1) % len(peoples)].name, peoples[num % len(peoples)].number) 
+        outstr += peoples[num % len(peoples)].name + ' >> ' + \
+            peoples[(num+1) % len(peoples)].name + '\n'
+        textResults(peoples[(num+1) % len(peoples)].name,
+                    peoples[num % len(peoples)].number)
     f.write(outstr)
+
 
 def textResults(name, number):
     textm = f'this is baileys robot, you are giving a gift to {name}'
-    response = plv.messages.create(src='19712063040', dst=number,text=textm)
+    response = plv.messages.create(src='19712063040', dst=number, text=textm)
     print(response)
 
 
@@ -30,7 +35,8 @@ def reads():
     with open('women') as f:
         for line in f.readlines():
             line = line.split(" ")
-            peoples.append(Person(line[0],line[1].strip('\n')))
+            peoples.append(Person(line[0], line[1].strip('\n')))
     printResults(peoples)
+
 
 reads()
